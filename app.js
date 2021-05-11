ac670ce8e4e44192be6244e51c49dbfa557ab77b
Lindsey Moss <llmodbls@gmail.com>
2021-05-11 11:58:59 -0400

    board built via DOM

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log('Mic check')
+
+// GLOBAL VARIABLES
+const body = document.querySelector('body')
+const main = document.querySelector('main')
+const playerBoard = document.querySelector('.playerboard')
+
+
+/////////////////////////////////////
+//// BOARD BUILD
+////////////////////////////////////
+const boardCells = [];
+let colCount = 0;
+let pointer = 0;
+let letters = ['_', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+for (i = 1; i <= 121; i++) {
+  let newCell = document.createElement('div');
+  playerBoard.appendChild(newCell)
+  newCell.innerText = `${letters[pointer]}${colCount}`
+  newCell.setAttribute('id', `${letters[pointer]}${colCount}`)
+  colCount++
+  if (i > 11 && colCount >= 2 && colCount <= 11) {
+    newCell.classList.add('playable')
+    boardCells.push(newCell)
+  }
+  if (i % 11 === 0) {
+    colCount = 0
+    pointer++
+  }
+}
+
+console.log(boardCells)
+// FUNCTIONS
+
+
+
+
+
+
+
+
+// EVENT LISTENERS
+
+
